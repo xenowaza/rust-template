c87d4ed838b23e915d074aa3402079b91226a482
xenowaza <162259590+xenowaza@users.noreply.github.com>
2024-03-16 11:00:01 +0000

    add file reader example

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::env;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let file = File::open(&args[1]);
+    let file = match file {
+        Ok(file) => file,
+        Err(error) => match error.kind() {
+            std::io::ErrorKind::NotFound => {
+                panic!("File not found: {}", error)
+            }
+            _ => {
+                panic!("Error opening file: {}", error)
+            }
+        },
+    };
+
+    let reader = BufReader::new(file);
+    for line in reader.lines() {
+        match line {
+            Ok(line) => println!("{}", line),
+            Err(error) => {
+                panic!("Error reading line: {}", error)
+            }
+        }
+    }
+}
